09e44142647b682a1f10aae38f6792441050230b
Bolun Andrei <telfagor@gmail.com>
2022-07-13 19:30:38 +0300

    Refactore the code

--- src/main/java/p1/NumberOfOccurrences.java
+++ src/main/java/p1/NumberOfOccurrences.java
@@ -8,41 +8,17 @@ public class NumberOfOccurrences {
     public boolean areOccurrencesEqual(String s) {
         Map<Character, Integer> map = new HashMap<>();
         for (char ch : s.toCharArray()) {
-            if (map.containsKey(ch)) {
-                int oldValue = map.get(ch);
-                map.put(ch, oldValue + 1);
-            } else {
-                map.put(ch, 1);
+            map.merge(ch, 1, (integer, integer2) -> integer + 1);
+        }
+
+        Integer firstValue = map.values().iterator().next();
+        for (Integer value : map.values()) {
+            if(!value.equals(firstValue)) {
+                return false;
             }
         }
 
-        int[] values = new int[map.size()];
-        int[] occurrences = new int[map.size()];
-
-        int k = 0;
-        for (Character ch : map.keySet()) {
-            values[k++] = (map.get(ch));
-        }
-
-        int counter = 0;
-        for (int i = 0; i < values.length; i++) {
-            for (int j = i + 1; j < values.length; j++) {
-                if (values[i] == values[j]) {
-                    counter++;
-                }
-            }
-            occurrences[i] = counter;
-            counter = 0;
-        }
-
-        boolean isGood = true;
-        for (int i = 0; i < occurrences.length - 1; i++) {
-            if (values[i] != values[i + 1]) {
-                isGood = false;
-            }
-        }
-
-        return isGood;
+        return true;
     }
 
     public static void main(String[] args) {
--- src/main/java/p1/RunningSum.java
+++ src/main/java/p1/RunningSum.java
@@ -11,6 +11,18 @@ public class RunningSum {
 
     public static int[] runningSum(int[] nums) {
         int[] arraySum = new int[nums.length];
+        arraySum[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            arraySum[i] = arraySum[i-1] + nums[i];
+        }
+
+        return arraySum;
+    }
+
+
+    public static int[] runningSum2(int[] nums) {
+        int[] arraySum = new int[nums.length];
 
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
